cc6e8314fc6df4a6d5f1c7d802c5db4054f38054
Nathan Walker <nwalk19@gmail.com>
2017-05-26 17:22:50 -0400

    update

--- index.php
+++ index.php
@@ -1,26 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
+  <head>
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="php code">
+    <meta name="description" content="">
     <meta name="author" content="nwalk">
-    <link rel="icon" href="">
-    <title>Home</title>
-    <link href="style_sheet.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
-    <div class="col-md-12 column" style="padding-top: 60px"></div>
-	<h4>
-	</h4>
-<p>
-<?php
+    <link rel="icon" href="#">
+    
+	
+	
+    <title>PHP</title>
 
+
+	<style>
+		h4 {
+			color:red;
+			font-weight:bold;
+		}
+	</style>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+	integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+
+  </head>
+  <body>
+<div class="container">
+	<div class="row">
+		<div class=col-md-12">
+			<button class="btn btn-success" id="submit">Button</button>
+		</div>
+	</div>
+</div>
+
+<?php
 //FOR LOOP
     for ($i=10; $i < 101; $i = $i + 10){
         echo $i . " ";
@@ -92,18 +107,20 @@
         default:
             echo "I don't know how much \$i is.";
     endswitch;
-
 ?>
+			
+    <script
+            src="https://code.jquery.com/jquery-2.2.4.min.js"
+            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+            crossorigin="anonymous"></script>
 
-</div>
-    <script src="https://code.jquery.com/jquery-3.1.0.min.js"
-          integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
-          crossorigin="anonymous">
-    </script>
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-         crossorigin="anonymous">
- </script>
 
-</body>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
+crossorigin="anonymous"></script>
+<script type="text/javascript">
+
+</script>
+  </body>
 </html>
